9ebf51d59f9123e753ced22a3225a9e4e2c1e4da
Marina López <marina.lopez.yap@gmail.com>
2024-09-05 12:27:54 +0200

    fix: fixed text, colors library and prototype examples

--- colors-library/src/plugin.ts
+++ colors-library/src/plugin.ts
@@ -4,7 +4,7 @@ penpot.ui.onMessage<string>((message) => {
   if (message === "create-color") {
     // TODO modify when it's possible to create a specific color https://tree.taiga.io/project/penpot/issue/8479
     // You can easily add a new color to your color library using this process.
-    penpot.library.local.createColor();
+    const newColorLibrary = penpot.library.local.createColor();
     // output
     // {
     //   "id": "f54fff93-7abe-80f4-8004-c64eba9e16c4",
@@ -15,15 +15,17 @@ penpot.ui.onMessage<string>((message) => {
     //   "gradient": null,
     //   "image": null
     // }
+    newColorLibrary.name = "primary";
+    newColorLibrary.color = "#7efff5";
   } else if (message === "read-colors") {
     // Take a look at the colors you have available in your library
     console.log(penpot.library.local.colors);
     // output
     // [{
     //   "id": "f54fff93-7abe-80f4-8004-c64eba9e16c4",
-    //   "name": "#000000",
+    //   "name": "#primary",
     //   "path": null,
-    //   "color": "#000000",
+    //   "color": "#7efff5",
     //   "opacity": 1,
     //   "gradient": null,
     //   "image": null
